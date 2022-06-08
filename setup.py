a00ded16cea01178bcd611f23cd501dee360bf5f
Paul Glushak <paul.glushak@forter.com>
2022-06-08 13:16:42 +0300

    Updates!

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="VNU",
+    version="0.1.0",
+    description='A small utility to automate "uninstalling" Node from Volta',
+    url="http://github.com/hxii",
+    author="Paul (hxii) Glushak",
+    author_email="paul@glushak.net",
+    license="MIT",
+    packages=["vnu"],
+    scripts=["bin/vnu"],
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "vnu = vnu.main:main",
+        ]
+    },
+)
